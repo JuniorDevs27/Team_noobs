71f1eedc7426e99382c0634ddc1225718d3aed32
JuniorDevs27 <161307337+JuniorDevs27@users.noreply.github.com>
2024-04-15 00:32:31 +0530

    Add files via upload

---
+++ ai doctor page/botsonic_script.js
@@ -0,0 +1,17 @@
+(function (w, d, s, o, f, js, fjs) {
+    w["botsonic_widget"] = o;
+    w[o] =
+      w[o] ||
+      function () {
+        (w[o].q = w[o].q || []).push(arguments);
+      };
+    (js = d.createElement(s)), (fjs = d.getElementsByTagName(s)[0]);
+    js.id = o;
+    js.src = f;
+    js.async = 1;
+    fjs.parentNode.insertBefore(js, fjs);
+  })(window, document, "script", "Botsonic", "https://widget.writesonic.com/CDN/botsonic.min.js");
+  Botsonic("init", {
+    serviceBaseUrl: "https://api-azure.botsonic.ai",
+    token: "d6060688-2f79-45d9-a06b-1f14e640ecac",
+  });
