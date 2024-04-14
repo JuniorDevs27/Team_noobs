fbf5d4c8a022a365a3a94ec95bcc8488c50fd586
JuniorDevs27 <161307337+JuniorDevs27@users.noreply.github.com>
2024-04-15 00:31:02 +0530

    Add files via upload

---
+++ Dr.KnowItAll/script.js
@@ -0,0 +1,50 @@
+
+function LoginPage() {
+ 
+  window.location.href = 'https://drknowitalllogin.netlify.app/';
+}
+function Feature1() {
+ 
+  window.location.href = 'https://multiple-disease-predictor-1.streamlit.app/';
+}
+function Feature2() {
+ 
+  window.location.href = 'https://ihavenoname.netlify.app/';
+}
+function Feature3() {
+ 
+  window.location.href = 'https://medicine-reminder-tawny.vercel.app/';
+}
+function Feature4() {
+ 
+  window.location.href = 'https://vdo-conf-final.vercel.app/';
+}
+function Feature5() {
+ 
+  window.location.href = 'https://chat-bot-jet-zeta.vercel.app/';
+}
+function blog1() {
+ 
+  window.location.href = 'https://www.verywellmind.com/the-best-mental-health-blogs-5205192';
+}
+function blog2() {
+ 
+  window.location.href = 'https://www.developgoodhabits.com/fitness-blogs/';
+}
+function blog3() {
+ 
+  window.location.href = 'https://relevant.software/blog/future-of-telemedicine/';
+}
+function blog4() {
+ 
+  window.location.href = 'https://www.healthcareexecutive.in/blog/empowering-voices';
+}
+function blog5() {
+ 
+  window.location.href = 'https://www.ibm.com/blog/the-benefits-of-ai-in-healthcare/';
+}
+function blog6() {
+ 
+  window.location.href = 'https://www.purdueglobal.edu/blog/student-life/valuable-health-wellness-blogs/';
+}
+
